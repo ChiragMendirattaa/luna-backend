08d6f962501641984462d32295a830dc22ec663d
Chirag Mendiratta <chiragmendiratta27@gmail.com>
2026-05-15 12:51:52 +0530

    deployment

--- src/main/java/com/lunacycle/config/SecurityConfig.java
+++ src/main/java/com/lunacycle/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://lunacycle.netlify.app"
+                "https://luna-cyclee.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
